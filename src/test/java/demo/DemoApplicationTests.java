0d7f912de06f7352c7274b35d3e5e4233757693c
Wei Wu <wei.wu05@sap.com>
2019-04-12 11:38:42 -0400

    Remove outdated tests

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,39 +1,5 @@
 package demo;
 
-import java.util.Arrays;
-
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.client.RestTemplate;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-// @SpringApplicationConfiguration(classes = DemoApplication.class)
-@WebAppConfiguration
-// @IntegrationTest
 public class DemoApplicationTests {
-
-	@Configuration
-	public static class RestClientConfiguration {
-
-		@Bean
-		RestTemplate restTemplate(ProtobufHttpMessageConverter hmc) {
-			return new RestTemplate(Arrays.asList(hmc));
-		}
-
-		@Bean
-		ProtobufHttpMessageConverter protobufHttpMessageConverter() {
-			return new ProtobufHttpMessageConverter();
-		}
-	}
-
-	@Autowired
-	private RestTemplate restTemplate;
-
-	private int port = 8080;
 
 }
